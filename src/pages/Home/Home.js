6e29e1388610a08860591e96d8bc9eee751c96cd
sungwoo <tjddn5242@gmail.com>
2020-08-17 12:48:51 +0900

    Organize by page(Home)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import TipRank from "../components/TipRank";
-import ReviewRank from "../components/ReviewRank";
-import UserRank from "../components/UserRank";
-import Recommend from "../components/Recommend";
+import TipRank from "./TipRank";
+import ReviewRank from "./ReviewRank";
+import UserRank from "./UserRank";
+import Recommend from "./Recommend";
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import AppLayout from "../components/AppLayout";
-import Home from "./Home";
+import Home from "./Home/Home";
 import AccountsRoutes from "./accounts";
 
 function Root() {
