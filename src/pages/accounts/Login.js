ae2d449a55eb36c601321b3a713398caf0fbf5b4
chanyoung <young9484825@gmail.com>
2020-08-29 21:43:54 +0900

    로그인 페이지 material ui 적용

--- src/pages/accounts/Login.js
+++ src/pages/accounts/Login.js
@@ -1,10 +1,43 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import Axios from "axios";
 import { useAppContext } from "../../store";
 import { setToken } from "store";
 import { useHistory, useLocation } from "react-router-dom";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    background: "#84E0CB",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#84E0CB",
+    },
+  },
+  input: {
+    boxShadow: `2px 2px 5px 1px rgba(0, 0, 0, 0.2)`,
+  },
+}));
 
 export default function Login() {
+  const classes = useStyles();
+
   const { dispatch } = useAppContext();
 
   const [inputs, setInputs] = useState({ username: "", password: "" });
@@ -44,16 +77,58 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <input type="text" name="username" onChange={onChange} />
-        </div>
-        <div>
-          <input type="password" name="password" onChange={onChange} />
-        </div>
-        <input type="submit" value="로그인" />
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h4">
+          로그인
+        </Typography>
+        <form className={classes.form} onSubmit={onSubmit}>
+          <TextField
+            className={classes.input}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            inputProps={{
+              minLength: 3,
+              maxLength: 20,
+            }}
+            onChange={onChange}
+          />
+          <TextField
+            className={classes.input}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            inputProps={{
+              minLength: 6,
+              maxLength: 20,
+            }}
+            onChange={onChange}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="inherit"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 }
