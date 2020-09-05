a2d9979bc64f1abee19204475ab6f9f3b9570c5b
jinsu-l <wlstn8738@naver.com>
2020-09-05 23:15:34 +0900

    django data 가져오기

--- src/pages/Home/UserRank.js
+++ src/pages/Home/UserRank.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-function UserRank(props) {
+function UserRank() {
+  const [userId, setUserId] = useState([]);
+
+  useEffect(() => {
+    let completed = false;
+
+    async function get(){
+      const result = await axios.get(
+        "http://127.0.0.1:8000/user"
+      );
+      if (!completed) setUserId(result.data);
+    }
+    get();
+    return () => {
+      completed =true;
+    };
+    })
+    const usersId = userId.map((id)=> id.user_id);
   return (
     <div>
       <h3>이달의 인기 멤버</h3>
       <p>
-        <img src={require("./1등.svg")} alt="1등 왕관 이미지" /> {props.first}
+        <img src={require("./1등.svg")} alt="1등 왕관 이미지" /> {usersId.slice(0,1)}
       </p>
       <p>
-        <img src={require("./2등.svg")} alt="2등 왕관 이미지" /> {props.second}
+        <img src={require("./2등.svg")} alt="2등 왕관 이미지" /> {usersId.slice(1,2)}
       </p>
       <p>
-        <img src={require("./3등.svg")} alt="3등 왕관 이미지" /> {props.third}
+        <img src={require("./3등.svg")} alt="3등 왕관 이미지" /> {usersId.slice(2,3)}
       </p>
     </div>
   );
